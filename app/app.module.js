bcefb06d810df63a5e7f2a1a18c73b8cbcfc69fc
reemhosny <reemhmahdy@gmail.com>
2022-02-05 15:00:12 +0200

    install vuex &  axios

--- app/app.module.js
+++ app/app.module.js
@@ -1,19 +1,28 @@
-import Vue from 'vue';
-import 'ngVue';
-import 'ngVue/build/plugins.js';
-import PerformancePageComponent from './pages/performance-page.vue';
-import PerformanceChartComponent from './components/vue-components/performance-chart.vue';
+import Vue from "vue";
+import store from "./store/index";
+import "ngVue";
+import "ngVue/build/plugins.js";
+import PerformancePageComponent from "./pages/performance-page.vue";
+import PerformanceChartComponent from "./components/vue-components/performance-chart.vue";
 
-angular.module('appModule', [
-  'ui.router',
-  'ngVue',
-  'ngVue.plugins',
-]);
+angular
+  .module("appModule", ["ui.router", "ngVue", "ngVue.plugins"])
+  .config(($ngVueProvider) => {
+    $ngVueProvider.setRootVueInstanceProps({ store: store });
+  });
 
-angular.module('appModule').directive('vPerformancePage', (createVueComponent) => {
-  return createVueComponent(Vue.component('performancePageComponent', PerformancePageComponent));
-});
+angular
+  .module("appModule")
+  .directive("vPerformancePage", (createVueComponent) => {
+    return createVueComponent(
+      Vue.component("performancePageComponent", PerformancePageComponent)
+    );
+  });
 
-angular.module('appModule').directive('vPerformanceChart', (createVueComponent) => {
-  return createVueComponent(Vue.component('performanceChartComponent', PerformanceChartComponent));
-});
+angular
+  .module("appModule")
+  .directive("vPerformanceChart", (createVueComponent) => {
+    return createVueComponent(
+      Vue.component("performanceChartComponent", PerformanceChartComponent)
+    );
+  });
---
+++ app/store/index.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import Vue from "vue";
+import Vuex from "vuex";
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  modules: {},
+});
+
+export default store;
